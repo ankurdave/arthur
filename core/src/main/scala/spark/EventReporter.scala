fd7b0ce32fc0b79f57a61b244b5b99a6ed172932
Ankur Dave <ankurdave@gmail.com>
2011-11-07 17:47:23 +0000

    Log RDDs to an event log at creation

--- core/src/main/scala/spark/EventReporter.scala
+++ core/src/main/scala/spark/EventReporter.scala
@@ -3,6 +3,7 @@ package spark
 import akka.actor.Actor
 import akka.actor.Actor._
 import akka.actor.ActorRef
+import java.io.{FileOutputStream, FileNotFoundException}
 
 sealed trait EventReporterMessage
 case class ReportException(exception: Throwable) extends EventReporterMessage
@@ -10,15 +11,27 @@ case class ReportRDDCreation(rdd: RDD[_]) extends EventReporterMessage
 case class ReportRDDChecksum(rdd: RDD[_], split: Split, checksum: Int) extends EventReporterMessage
 
 class EventReporterActor extends Actor with Logging {
+  val eventLog =
+    try {
+      Some(new FileOutputStream(System.getProperty("spark.logging.eventLog"), true))
+    } catch {
+      case e: FileNotFoundException =>
+        logWarning("Can't write to %s: %s".format(System.getProperty("spark.logging.eventLog"), e))
+        None
+    }
+
   def receive = {
     case ReportException(exception) =>
       logInfo("Received exception: %s".format(exception))
 
     case ReportRDDCreation(rdd) =>
-      logInfo("Received RDD creation: %s".format(rdd))
+      for (l <- eventLog) {
+        // TODO: avoid serializing the same RDD once for each time it's referenced as a dependency
+        l.write(Utils.serialize(rdd))
+      }
 
     case ReportRDDChecksum(rdd, split, checksum) =>
-      logInfo("Received checksum for split %d of RDD %s: %d".format(split.index, rdd, checksum))
+      logInfo("Received checksum for split %d of RDD %s: %d".format(split.index, rdd.id, checksum))
   }
 }
 
