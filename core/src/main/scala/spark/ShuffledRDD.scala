7a243b59dd9338345ef0657f57beab135c356859
Ankur Dave <ankurdave@gmail.com>
2012-05-07 12:34:53 -0700

    Preserve partitioner in ShuffledRDD.tagged

--- core/src/main/scala/spark/ShuffledRDD.scala
+++ core/src/main/scala/spark/ShuffledRDD.scala
@@ -28,15 +28,17 @@ class ShuffledRDD[K, V, C](
   override def mapDependencies(g: RDD ~> RDD) = new ShuffledRDD(g(parent), aggregator, part)
 
   override def tagged(tagger: RDDTagger): RDD[Tagged[(K, C)]] = {
-    val taggedParent: RDD[(K, Tagged[V])] = tagger(parent).map {
+    val taggedParent: RDD[(K, Tagged[V])] = new SamePartitionMappedRDD[(K, Tagged[V]), Tagged[(K, V)]](tagger(parent), {
       case Tagged((k, v), tag) => (k, Tagged(v, tag))
-    }
+    })
     val tagAggregator: Aggregator[K, Tagged[V], Tagged[C]] = new Aggregator(
       { case Tagged(v, tag) => Tagged(aggregator.createCombiner(v), tag) },
       { case (Tagged(c, cTag), Tagged(v, vTag)) => Tagged(aggregator.mergeValue(c, v), cTag | vTag) },
       { case (Tagged(c1, c1Tag), Tagged(c2, c2Tag)) => Tagged(aggregator.mergeCombiners(c1, c2), c1Tag | c2Tag) })
     val shuffled: RDD[(K, Tagged[C])] = new ShuffledRDD(taggedParent, tagAggregator, part)
-    shuffled.map {case (k, Tagged(c, tag)) => Tagged((k, c), tag)}
+    new SamePartitionMappedRDD[Tagged[(K, C)], (K, Tagged[C])](shuffled, {
+      case (k, Tagged(c, tag)) => Tagged((k, c), tag)
+    })
   }
 
   override def compute(split: Split): Iterator[(K, C)] = {
--- core/src/main/scala/spark/Tagged.scala
+++ core/src/main/scala/spark/Tagged.scala
@@ -2,11 +2,21 @@ package spark
 
 import scala.collection.immutable
 
-case class Tagged[+A](val elem: A, val tag: immutable.HashSet[Int]) {
+case class Tagged[+A](val elem: A, val tag: immutable.HashSet[Int]) extends Product2[Any, Any] {
   def map[B](f: A => B): Tagged[B] = Tagged(f(elem), tag)
 
   def flatMap[B](f: A => Traversable[B]) =
     for (newElem <- f(elem)) yield Tagged(newElem, tag)
+
+  override def _1: Any = elem match {
+    case pair: Product2[_,_] => pair._1
+    case _ => null
+  }
+  override def _2: Any = elem match {
+    case pair: Product2[_,_] => Tagged(pair._2, tag)
+    case _ => null
+  }
+  override def canEqual(that: Any) = that.isInstanceOf[Tagged[_]]
 }
 
 object Tagged {
@@ -35,5 +45,21 @@ class UniquelyTaggedRDD[T](prev: RDD[T]) extends RDD[Tagged[T]](prev.context) {
 
   private val numSplits = splits.length
 
+  reportCreation()
+}
+
+/** Maps the prev RDD assuming that f keeps the element in the same partition. This allows it to reuse the partitioner. */
+class SamePartitionMappedRDD[U: ClassManifest, T: ClassManifest](
+    prev: RDD[T],
+    f: T => U)
+  extends RDD[U](prev.context) {
+
+  override def splits = prev.splits
+  override val dependencies = List(new OneToOneDependency(prev))
+  override def mapDependencies(g: RDD ~> RDD) = new SamePartitionMappedRDD(g(prev), f)
+  override val partitioner = prev.partitioner
+  override def compute(split: Split) = prev.iterator(split).map(f)
+  override def tagged(tagger: RDDTagger) =
+    new SamePartitionMappedRDD(tagger(prev), (tt: Tagged[T]) => tt.map(f))
   reportCreation()
 }
