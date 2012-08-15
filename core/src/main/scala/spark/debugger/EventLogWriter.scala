ad1178cd96fa65e4c0f6a80eb9bde81f8c48eb10
Ankur Dave <ankurdave@gmail.com>
2012-08-14 20:04:29 -0700

    Debug to a temp file by default

--- core/src/main/scala/spark/debugger/EventLogWriter.scala
+++ core/src/main/scala/spark/debugger/EventLogWriter.scala
@@ -1,6 +1,7 @@
 package spark.debugger
 
 import java.io._
+import java.util.UUID
 
 import scala.collection.immutable
 import scala.collection.mutable
@@ -9,21 +10,27 @@ import scala.collection.mutable.ArrayBuffer
 import spark.Logging
 
 /**
- * Writes events to an event log on disk.
+ * Writes events to an event log on disk. Logging is thread-safe.
  */
-class EventLogWriter {
+class EventLogWriter extends Logging {
   private var eventLog: Option[EventLogOutputStream] = None
-  setEventLogPath(Option(System.getProperty("spark.arthur.logPath")))
+  setEventLogPath(Option(System.getProperty("spark.debugger.logPath")))
   private val checksumVerifier = new ChecksumVerifier
   val subscribers = new ArrayBuffer[EventLogEntry => Unit]
 
   def setEventLogPath(eventLogPath: Option[String]) {
-    eventLog =
-      for {
-        elp <- eventLogPath
-        file = new File(elp)
-        if !file.exists
-      } yield new EventLogOutputStream(new FileOutputStream(file))
+    val path = eventLogPath.getOrElse {
+      val dir = System.getProperty("spark.local.dir", System.getProperty("java.io.tmpdir"))
+      new File(dir, "spark-debugger-event-log-" + UUID.randomUUID).getPath
+    }
+    val file = new File(path)
+    if (!file.exists) {
+      logInfo("Writing to event log %s".format(path))
+      eventLog = Some(new EventLogOutputStream(new FileOutputStream(file)))
+    } else {
+      logWarning("Event log %s already exists".format(path))
+      eventLog = None
+    }
   }
 
   def subscribe(callback: EventLogEntry => Unit) {
